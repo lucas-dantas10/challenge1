dacd210806093b8a2bd6936c13bc31704a7a6fbb
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2024-01-08 00:31:11 -0300

    Add project files.

---
+++ challenge1/Controllers/JsonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace challenge1.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    [Produces("application/json")]
+    public class JsonController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Dictionary<string, string> response = new Dictionary<string, string>
+            {
+                {"message", "Olá, Mundo!" }
+            };
+
+            return Json(response);
+        }
+    }
+}
